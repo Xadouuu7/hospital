75e559b894d15fe03767fdc75b07e3face13e8f1
Anderson <a.perez12@sapalomera.cat>
2024-04-06 01:41:41 +0200

    comprobacion de que el usuario ya existe

--- python/Bloque Conexion y Login/funciones.py
+++ python/Bloque Conexion y Login/funciones.py
@@ -13,7 +13,7 @@ def menuInicial():
     os.system('cls')
     tituloMenu('Menu Login')
     print("1. Inicio de sesion")
-    print("2. Registrars-se", end='\n\n\n')
+    print("2. Registrarse", end='\n\n\n')
     print("Escoger una opcion: ", end='')
     respuesta = input()
     if '1' == respuesta:
@@ -29,7 +29,7 @@ def menuIniciarSesion():
     print('Contraseña: ', end='')
     contraseña = input()
     conectarBaseDatos(usuario,contraseña)
-    print("Bieeeen")
+    
 
 def menuRegistrarse():
     os.system('cls')
@@ -58,31 +58,41 @@ def conectarBaseDatos(usuario = 'postgres', contraseña = 'postgres'):
 
 def crearUsuario(usuario, contraseña):
     try:
-        ## CREACION DEL USUARIO EN POSTGRES
-        conn, cursor = conectarBaseDatos()
-        query = f'CREATE ROLE "{usuario}" LOGIN PASSWORD %s'
-        cursor.execute(query,(contraseña,))
+        if not comprobarUsuario(usuario):
+            ## CREACION DEL USUARIO EN POSTGRES
+            conn, cursor = conectarBaseDatos()
+            query = f'CREATE ROLE "{usuario}" LOGIN PASSWORD %s'
+            cursor.execute(query,(contraseña,))
 
-        ## CREACION DEL USUARIO EN CSV
-        ### SALTS
-        salt_usuario = (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20)))
-        salt_contraseña = (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20)))
+            ## CREACION DEL USUARIO EN CSV
+            ### SALTS
+            salt_usuario = (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20)))
+            salt_contraseña = (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20)))
 
-        ### HASHING
-        usuario = hashlib.sha256(usuario.encode() + salt_usuario.encode()).hexdigest()
-        contraseña = hashlib.sha256(contraseña.encode() + salt_contraseña.encode()).hexdigest()
+            ### HASHING
+            usuario = hashlib.sha256(usuario.encode() + salt_usuario.encode()).hexdigest()
+            contraseña = hashlib.sha256(contraseña.encode() + salt_contraseña.encode()).hexdigest()
 
-        ### WRITING TO CSV
-        fichero = readUsuario()
-        with open("usuarios.csv", 'w', newline='',encoding="UTF-8") as file:
-            writer = csv.DictWriter(file,['salt_usuario','usuario','salt_contraseña','contraseña'], delimiter=';')
-            writer.writeheader()
-            for row in fichero:
-                writer.writerow(row)
-            writer.writerow({'salt_usuario':salt_usuario, 'usuario': usuario, 'salt_contraseña':salt_contraseña, 'contraseña':contraseña})
+            ### WRITING TO CSV
+            fichero = readUsuario()
+            with open("usuarios.csv", 'w', newline='',encoding="UTF-8") as file:
+                writer = csv.DictWriter(file,['salt_usuario','usuario','salt_contraseña','contraseña'], delimiter=';')
+                writer.writeheader()
+                for row in fichero:
+                    writer.writerow(row)
+                writer.writerow({'salt_usuario':salt_usuario, 'usuario': usuario, 'salt_contraseña':salt_contraseña, 'contraseña':contraseña})
     except Exception as error:
         print("Ha ocurrido un error:", error)
+        input("Enter para continuar: ")
 
+def comprobarUsuario(usuario):
+    conn, cursor = conectarBaseDatos()
+    query = "SELECT * FROM pg_catalog.pg_user WHERE usename = %s"
+    cursor.execute(query, (usuario,))
+    if cursor.fetchone():
+        print("\nEl usuario ya existe.")
+        return True
+    
 def readUsuario():
     fichero = []
     with open("usuarios.csv", 'a',encoding="UTF-8") as file:
