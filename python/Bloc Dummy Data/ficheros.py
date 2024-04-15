53aaff5564191a9e34b8df62cc22247ea3119b52
Isaac <isaky032005@gmail.com>
2024-04-15 19:43:06 +0200

    hola

--- python/Bloc Dummy Data/ficheros.py
+++ python/Bloc Dummy Data/ficheros.py
@@ -1,11 +1,9 @@
-import csv 
+import csv
 
 def readCiudad():
-    fichero = []
-    with open("ciudad.csv", 'a',encoding="UTF-8") as file:
-        pass
-    with open("ciudad.csv", 'r',encoding="UTF-8") as file:
-        reader = csv.DictReader(file, delimiter=';')
+    fichero = {}
+    with open("ciudad.csv", 'r',encoding="UTF-8") as f:
+        reader = csv.DictReader(f, delimiter=';')
         for row in reader:
             fichero.append(row)
     return fichero
