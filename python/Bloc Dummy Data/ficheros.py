b3c53dd4d64278b547348e9f790148c949108ffc
Xadouuu7 <158314789+Xadouuu7@users.noreply.github.com>
2024-04-15 19:22:07 +0200

    ASD

--- python/Bloc Dummy Data/ficheros.py
+++ python/Bloc Dummy Data/ficheros.py
@@ -1,1 +1,13 @@
 import csv 
+
+def readCiudad():
+    fichero = []
+    with open("ciudad.csv", 'a',encoding="UTF-8") as file:
+        pass
+    with open("ciudad.csv", 'r',encoding="UTF-8") as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            fichero.append(row)
+    return fichero
+
+readCiudad()
