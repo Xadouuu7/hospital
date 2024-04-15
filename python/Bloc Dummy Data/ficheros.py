63edd01088cc22d39daf929882cb20bc6df333f2
Isaac <isaky032005@gmail.com>
2024-04-15 20:20:36 +0200

    FicheroSinDuplicados

--- python/Bloc Dummy Data/ficheros.py
+++ python/Bloc Dummy Data/ficheros.py
@@ -1,11 +1,30 @@
 import csv
 
-def readCiudad():
-    fichero = {}
+def quitarDuplicados():
+
+    fichero = []
+    nombreCiudades = []
+
+    ## LEEMOS EL FICHERO DONDE HAY CIUDADES DUPLICADAS Y NOS GUARDAMOS SOLO LOS NOMBRES SIN DUPLICADOS
     with open("ciudad.csv", 'r',encoding="UTF-8") as f:
         reader = csv.DictReader(f, delimiter=';')
         for row in reader:
-            print(row)
-    return fichero
+            if row["nombre"] not in nombreCiudades:
+                nombreCiudades.append(row["nombre"])
 
-readCiudad()
+    ## COMPROBAMOS QUE EL NOMBRE ESTÁ EN LA LISTA DE NOMBRES, SI ES TRUE ELIMINA EL NOMBRE DE LA LISTA Y AÑADE LA FILA DEL CSV A UNA LISTA
+    with open("ciudad.csv", 'r',encoding="UTF-8") as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for row in reader:
+            if row["nombre"] in nombreCiudades:
+                fichero.append(row)
+                nombreCiudades.remove(row["nombre"])
+
+    ## BORRAREMOS TODO EL FICHERO Y LE AÑADIREMOS LOS REGISTROS QUE HEMOS AÑADIDO PREVIAMENTE EN LA LISTA
+    with open("ciudad.csv",'w',encoding='UTF-8',newline='') as f:
+        writer = csv.DictWriter(f,['codigo_postal','nombre'],delimiter=';')
+        writer.writeheader()
+        for a in fichero:
+            writer.writerow({'codigo_postal':a["codigo_postal"],'nombre':a["nombre"]})
+
+quitarDuplicados()
