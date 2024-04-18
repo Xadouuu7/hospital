2ed14bd29c7652e72f3db98d0e8e6071c48ce70b
Mar <m.manzano@sapalomera.cat>
2024-04-18 22:59:36 +0200

    Seguridad, vistas y SQL cambiado

--- postgres/Esquema Seguridad/SQLSeguridad.sql
+++ postgres/Esquema Seguridad/SQLSeguridad.sql
@@ -2,13 +2,13 @@
 CREATE ROLE medico;
 GRANT CONNECT ON DATABASE hospital TO medico;
 GRANT USAGE SCHEMA public TO medico;
-GRANT SELECT ON view_visita TO medico;
+GRANT SELECT ON view_visita TO medico;      
 GRANT SELECT ON view_diagnostico TO medico;
 GRANT SELECT ON view_receta TO medico;
-GRANT SELECT ON view_medicamento TO medico;
-GRANT SELECT ON view_patologia TO medico;
 GRANT SELECT ON view_reserva_habitacion TO medico;
 GRANT SELECT ON view_reserva_quirofano TO medico;
+GRANT SELECT ON medicamento TO medico;   -- Aquí he puesto que sea la tabla en sí porque no hay nada más importante como para crear una vista con inner joins.
+GRANT SELECT ON patologia TO medico;     -- Lo mismo que el anterior
 GRANT INSERT ON visita TO medico;
 GRANT INSERT ON diagnostico TO medico;
 GRANT INSERT ON receta TO medico;
@@ -18,9 +18,10 @@ CREATE ROLE administrativo;
 GRANT CONNECT ON DATABASE hospital TO administrativo;
 GRANT USAGE SCHEMA public TO administrativo;
 GRANT SELECT ON view_visita TO administrativo;
-GRANT SELECT ON view_horariomedico TO administrativo;
 GRANT SELECT ON view_reserva_quirofano TO administrativo;
 GRANT SELECT ON view_reserva_habitacion TO administrativo;
+GRANT SELECT ON view_agenda TO administrativo;
+GRANT INSERT ON reserva_visita TO administrativo;
 GRANT INSERT ON visita TO administrativo;
 GRANT INSERT ON reserva_quirofano TO administrativo;
 GRANT INSERT ON reserva_habitacion TO administrativo;
@@ -31,7 +32,7 @@ GRANT CONNECT ON DATABASE hospital TO cientifico;
 GRANT USAGE SCHEMA public TO cientifico;
 GRANT SELECT ON view_prueba TO cientifico;
 GRANT SELECT ON view_diagnostico TO cientifico;
-GRANT SELECT ON view_inv_laboratorio TO cientifico;
+GRANT SELECT ON inv_laboratorio TO cientifico;
 GRANT INSERT ON prueba TO cientifico;
 GRANT INSERT ON inv_laboratorio TO cientifico;
 
@@ -40,24 +41,19 @@ CREATE ROLE enfermero;
 GRANT CONNECT ON DATABASE hospital TO enfermero;
 GRANT USAGE SCHEMA public TO enfermero;
 GRANT SELECT ON view_receta TO enfermero;
-GRANT SELECT ON view_inv_general TO enfermero;
-GRANT SELECT ON view_material_general TO enfermero;
-GRANT SELECT ON view_triaje TO enfermero;
-GRANT INSERT ON inv_material_general TO enfermero;
-GRANT INSERT ON triaje TO enfermero;
+GRANT SELECT ON material_general TO enfermero;
+GRANT SELECT, INSERT ON triaje TO enfermero;
+GRANT SELECT, INSERT ON inv_material_general TO enfermero;
 
 --- FARMACÉUTICO
 CREATE ROLE farmaceutico;
 GRANT CONNECT ON DATABASE hospital TO farmaceutico;
 GRANT USAGE SCHEMA public TO farmaceutico;
 GRANT SELECT ON view_receta TO farmaceutico;
-GRANT SELECT ON view_medicamento TO farmaceutico;
-GRANT SELECT ON view_inv_medicamento TO farmaceutico;
-GRANT INSERT ON inv_medicamento TO farmaceutico;
+GRANT SELECT, INSERT ON inv_medicamento TO farmaceutico;
 
 --- RH
 CREATE ROLE recursos_humanos;
 GRANT CONNECT ON DATABASE hospital TO recursos_humanos;
 GRANT USAGE SCHEMA public TO recursos_humanos;
-GRANT SELECT ON view_empleados TO recursos_humanos;
-GRANT INSERT ON empleados TO recursos_humanos;
+GRANT SELECT, INSERT ON empleados TO recursos_humanos;
---
+++ postgres/Esquema Seguridad/SQLVistas.sql
@@ -0,0 +1,85 @@
+--- MÉDICO
+
+CREATE VIEW view_visita AS 
+    SELECT
+        CONCAT(pepa.nombre, pepa.apellido1, pepa.apellido2),
+        vi.fecha_hora, vi.motivo_visita,
+        CONCAT(peme.nombre, peme.apellido1)
+    FROM visita vi 
+    INNER JOIN paciente pa ON vi.id_paciente = pa.tarjeta_sanitaria
+    INNER JOIN medico me ON vi.id_medico = me.id_empleado
+    INNER JOIN persona pepa ON pepa.dni_nie = pa.dni_nie
+    INNER JOIN persona peme ON peme.dni_nie = me.dni_nie;
+
+CREATE VIEW view_diagnostico AS 
+    SELECT
+        pepa.nombre,
+        pepa.apellido1,
+        pepa.apellido2,
+        diag.descripcion,
+        pato.nombre
+    FROM diagnostico diag
+    INNER JOIN patologia pato ON pato.id_patologia = diag.id_patologia
+    INNER JOIN persona pepa ON pepa.dni_nie = pa.dnie_nie;
+
+CREATE VIEW view_receta AS 
+    SELECT
+        CONCAT(pepa.nombre, pepa.apellido1, pepa.apellido2),
+        medi.nombre,
+        rece.fecha_hora,
+        rece.dosis,
+        CONCAT(peme.nombre, peme.apellido)
+    FROM receta rece
+    INNER JOIN medicamento medi ON medi.id_medicamento = rece.id_medicamento
+    INNER JOIN diagnostico diag ON diagnostico.id_diagnostico = rece.diagnostico 
+    INNER JOIN paciente pa ON rece.id_paciente = pa.tarjeta_sanitaria
+    INNER JOIN medico me ON rece.id_medico = me.id_empleado
+    INNER JOIN persona pepa ON pepa.dni_nie = pa.dni_nie
+    INNER JOIN persona peme ON peme.dni_nie = me.dni_nie;
+
+CREATE VIEW view_reserva_habitacion AS 
+    SELECT
+        CONCAT(pepa.nombre, pepa.apellido1, pepa.apellido2),
+        reservhab.num_habitacion,
+        reservhab.num_planta,
+        rerservhab.fecha_entrada_salida
+    FROM reserva_habitacion reservhab
+    INNER JOIN paciente pa ON reservhab.id_paciente = pa.tarjeta_sanitaria
+    INNER JOIN persona pepa ON pepa.dni_nie = pa.dni_nie;
+
+CREATE VIEW view_reserva_quirofano AS 
+    SELECT 
+        CONCAT(pepa.nombre, pepa.apellido1, pepa.apellido2), 
+        reservquiro.num_quirofano, 
+        reservquiro.num_planta, 
+        CONCAT(peme.nombre, peme.apellido1), 
+        reservquiro.fecha_entrada 
+    FROM reserva_quirofano 
+    INNER JOIN medico me ON reservquiro.id_medico = me.id_empleado
+    INNER JOIN persona peme ON peme.dni_nie = me.dni_nie
+    INNER JOIN paciente pa ON reservquiro.id_paciente = pa.tarjeta_sanitaria
+    INNER JOIN persona pepa ON pepa.dni_nie = pa.dni_nie;
+
+--- ADMINISTRATIVO
+--- tengo que pensar como hacer para las reservas de las visitas
+
+--- CIENTÍFICO
+CREATE VIEW view_prueba AS
+    SELECT 
+        CONCAT(pepa.nombre, pepa.apellido1, pepa.apellido2),
+        p.tipo
+    FROM prueba p 
+    INNER JOIN diagnostico d ON d.id_diagnostico=p.id_diagnostico
+    INNER JOIN visita v ON d.id_diagnostico=v.id_diagnostico
+    INNER JOIN paciente pa ON reservquiro.id_paciente = pa.tarjeta_sanitaria
+    INNER JOIN persona peme ON peme.dni_nie = me.dni_nie;
+
+--- ENFERMERO
+CREATE VIEW view_triaje AS
+    SELECT 
+
+
+--- FARMACÉUTICO
+
+
+--- RH
