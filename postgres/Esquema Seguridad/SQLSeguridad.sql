2e1b5c13d7442b17675bd6bdc24c8f090a66987d
Xadouuu7 <158314789+Xadouuu7@users.noreply.github.com>
2024-04-21 18:42:56 +0200

    Update SQLSeguridad.sql

--- postgres/Esquema Seguridad/SQLSeguridad.sql
+++ postgres/Esquema Seguridad/SQLSeguridad.sql
@@ -1,7 +1,7 @@
 --- MÉDICO
 CREATE ROLE medico;
 GRANT CONNECT ON DATABASE hospital TO medico;
-GRANT USAGE SCHEMA public TO medico;
+GRANT USAGE ON SCHEMA public TO medico;
 GRANT SELECT ON view_visita TO medico;      
 GRANT SELECT ON view_diagnostico TO medico;
 GRANT SELECT ON view_receta TO medico;
@@ -16,7 +16,7 @@ GRANT INSERT ON receta TO medico;
 --- ADMINISTRATIVO
 CREATE ROLE administrativo;
 GRANT CONNECT ON DATABASE hospital TO administrativo;
-GRANT USAGE SCHEMA public TO administrativo;
+GRANT USAGE ON SCHEMA public TO administrativo;
 GRANT SELECT ON view_visita TO administrativo;
 GRANT SELECT ON view_reserva_quirofano TO administrativo;
 GRANT SELECT ON view_reserva_habitacion TO administrativo;
@@ -29,7 +29,7 @@ GRANT INSERT ON reserva_habitacion TO administrativo;
 --- CIENTÍFICO
 CREATE ROLE científico;
 GRANT CONNECT ON DATABASE hospital TO cientifico;
-GRANT USAGE SCHEMA public TO cientifico;
+GRANT USAGE ON SCHEMA public TO cientifico;
 GRANT SELECT ON view_prueba TO cientifico;
 GRANT SELECT ON view_diagnostico TO cientifico;
 GRANT SELECT ON inv_laboratorio TO cientifico;
@@ -39,7 +39,7 @@ GRANT INSERT ON inv_laboratorio TO cientifico;
 --- ENFERMERO
 CREATE ROLE enfermero;
 GRANT CONNECT ON DATABASE hospital TO enfermero;
-GRANT USAGE SCHEMA public TO enfermero;
+GRANT USAGE ON SCHEMA public TO enfermero;
 GRANT SELECT ON view_receta TO enfermero;
 GRANT SELECT ON material_general TO enfermero;
 GRANT SELECT, INSERT ON triaje TO enfermero;
@@ -48,12 +48,16 @@ GRANT SELECT, INSERT ON inv_material_general TO enfermero;
 --- FARMACÉUTICO
 CREATE ROLE farmaceutico;
 GRANT CONNECT ON DATABASE hospital TO farmaceutico;
-GRANT USAGE SCHEMA public TO farmaceutico;
+GRANT USAGE ON SCHEMA public TO farmaceutico;
 GRANT SELECT ON view_receta TO farmaceutico;
 GRANT SELECT, INSERT ON inv_medicamento TO farmaceutico;
 
 --- RH
 CREATE ROLE recursos_humanos;
 GRANT CONNECT ON DATABASE hospital TO recursos_humanos;
-GRANT USAGE SCHEMA public TO recursos_humanos;
+GRANT USAGE ON SCHEMA public TO recursos_humanos;
 GRANT SELECT, INSERT ON empleados TO recursos_humanos;
+
+--- INFORMATICO
+CREATE ROLE informatico;
+GRANT ALL PRIVILEGES ON DATABASE hospital TO informatico;
